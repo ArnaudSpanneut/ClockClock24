8466f78f196f0e5158cadc43d13f24a9ed7c6078
ArnaudSpanneut <arnaud.spanneut@gmail.com>
2018-11-27 08:32:25 +0100

    feat(SHAPES): Create new shapes

--- src/constants/shapes.js
+++ src/constants/shapes.js
@@ -5,76 +5,73 @@ const ALL_OBLIQUES = [ // OBLIQUE
   [utils.OBLIQUE_LINE, utils.OBLIQUE_LINE],
   [utils.OBLIQUE_LINE, utils.OBLIQUE_LINE],
 ];
-const ALL_DEACTIVATE_1 = [
-  [utils.DEACTIVATE_TOP_RIGHT, utils.DEACTIVATE_BOTTOM_RIGHT],
-  [utils.DEACTIVATE_TOP_RIGHT, utils.DEACTIVATE_BOTTOM_RIGHT],
-  [utils.DEACTIVATE_TOP_RIGHT, utils.DEACTIVATE_BOTTOM_RIGHT],
+const ALL_DEACTIVATE = [
+  [utils.DEACTIVATE_BOTTOM_LEFT, utils.DEACTIVATE_BOTTOM_LEFT],
+  [utils.DEACTIVATE_BOTTOM_LEFT, utils.DEACTIVATE_BOTTOM_LEFT],
+  [utils.DEACTIVATE_BOTTOM_LEFT, utils.DEACTIVATE_BOTTOM_LEFT],
 ];
-const ALL_DEACTIVATE_2 = [
-  [utils.DEACTIVATE_TOP_RIGHT, utils.DEACTIVATE_BOTTOM_RIGHT],
-  [utils.DEACTIVATE_TOP_LEFT, utils.DEACTIVATE_TOP_LEFT],
-  [utils.DEACTIVATE_TOP_RIGHT, utils.DEACTIVATE_BOTTOM_RIGHT],
+const OBLIQUES = [
+  ALL_OBLIQUES,
+  ALL_OBLIQUES,
+  ALL_OBLIQUES,
+  ALL_OBLIQUES,
+];
+const DEACTIVATE = [
+  ALL_DEACTIVATE,
+  ALL_DEACTIVATE,
+  ALL_DEACTIVATE,
+  ALL_DEACTIVATE,
+];
+const WIND = [
+  [
+    [{ hours: 95, minutes: 95 }, { hours: 100, minutes: 100 }],
+    [{ hours: 90, minutes: 90 }, { hours: 90, minutes: 90 }],
+    [{ hours: 85, minutes: 85 }, { hours: 80, minutes: 80 }],
+  ],
+  [
+    [{ hours: 110, minutes: 110 }, { hours: 150, minutes: 150 }],
+    [{ hours: 90, minutes: 90 }, { hours: 90, minutes: 90 }],
+    [{ hours: 70, minutes: 70 }, { hours: 30, minutes: 30 }],
+  ],
+  [
+    [{ hours: 210, minutes: 210 }, { hours: 250, minutes: 250 }],
+    [{ hours: 270, minutes: 270 }, { hours: 270, minutes: 270 }],
+    [{ hours: 330, minutes: 330 }, { hours: 290, minutes: 290 }],
+  ],
+  [
+    [{ hours: 260, minutes: 260 }, { hours: 265, minutes: 265 }],
+    [{ hours: 270, minutes: 270 }, { hours: 270, minutes: 270 }],
+    [{ hours: 280, minutes: 280 }, { hours: 275, minutes: 275 }],
+  ],
+];
+const SQUARES = [
+  [
+    [{ hours: 225, minutes: 225 }, { hours: 225, minutes: 225 }],
+    [{ hours: 135, minutes: 45 }, { hours: 135, minutes: 45 }],
+    [{ hours: 315, minutes: 315 }, { hours: 315, minutes: 315 }],
+  ],
+  [
+    [{ hours: 225, minutes: 225 }, { hours: 225, minutes: 225 }],
+    [{ hours: 135, minutes: 45 }, { hours: 135, minutes: 45 }],
+    [{ hours: 315, minutes: 315 }, { hours: 315, minutes: 315 }],
+  ],
+  [
+    [{ hours: 135, minutes: 135 }, { hours: 135, minutes: 135 }],
+    [{ hours: 315, minutes: 225 }, { hours: 315, minutes: 225 }],
+    [{ hours: 45, minutes: 45 }, { hours: 45, minutes: 45 }],
+  ],
+  [
+    [{ hours: 135, minutes: 135 }, { hours: 135, minutes: 135 }],
+    [{ hours: 315, minutes: 225 }, { hours: 315, minutes: 225 }],
+    [{ hours: 45, minutes: 45 }, { hours: 45, minutes: 45 }],
+  ],
 ];
 
 const SHAPES = [
-  [
-    ALL_OBLIQUES,
-    ALL_OBLIQUES,
-    ALL_OBLIQUES,
-    ALL_OBLIQUES,
-  ], [
-    ALL_DEACTIVATE_1,
-    ALL_DEACTIVATE_1,
-    ALL_DEACTIVATE_1,
-    ALL_DEACTIVATE_1,
-  ], [
-    ALL_DEACTIVATE_2,
-    ALL_DEACTIVATE_2,
-    ALL_DEACTIVATE_2,
-    ALL_DEACTIVATE_2,
-  ], [ // Square
-    [
-      [utils.VERTICAL_LINE, utils.ANGLE_BOTTOM_RIGHT],
-      [utils.VERTICAL_LINE, utils.VERTICAL_LINE],
-      [utils.VERTICAL_LINE, utils.VERTICAL_LINE],
-    ],
-    [
-      [utils.HORIZONTAL_LINE, utils.HORIZONTAL_LINE],
-      [utils.ANGLE_BOTTOM_RIGHT, utils.HORIZONTAL_LINE],
-      [utils.VERTICAL_LINE, utils.ANGLE_BOTTOM_RIGHT],
-    ],
-    [
-      [utils.HORIZONTAL_LINE, utils.HORIZONTAL_LINE],
-      [utils.HORIZONTAL_LINE, utils.ANGLE_BOTTOM_LEFT],
-      [utils.ANGLE_BOTTOM_LEFT, utils.VERTICAL_LINE],
-    ],
-    [
-      [utils.ANGLE_BOTTOM_LEFT, utils.VERTICAL_LINE],
-      [utils.VERTICAL_LINE, utils.VERTICAL_LINE],
-      [utils.VERTICAL_LINE, utils.VERTICAL_LINE],
-    ],
-  ], [ // Lol
-    [
-      [utils.VERTICAL_LINE, utils.BOTTOM],
-      [utils.VERTICAL_LINE, utils.VERTICAL_LINE],
-      [utils.VERTICAL_LINE, utils.ANGLE_TOP_RIGHT],
-    ],
-    [
-      [utils.DEACTIVATE_TOP_RIGHT, utils.ANGLE_BOTTOM_RIGHT],
-      [utils.DEACTIVATE_TOP_RIGHT, utils.VERTICAL_LINE],
-      [utils.LEFT, utils.ANGLE_TOP_RIGHT],
-    ],
-    [
-      [utils.ANGLE_BOTTOM_LEFT, utils.BOTTOM],
-      [utils.VERTICAL_LINE, utils.VERTICAL_LINE],
-      [utils.ANGLE_TOP_LEFT, utils.ANGLE_TOP_RIGHT],
-    ],
-    [
-      [utils.DEACTIVATE_TOP_RIGHT, utils.VERTICAL_LINE],
-      [utils.DEACTIVATE_TOP_RIGHT, utils.VERTICAL_LINE],
-      [utils.LEFT, utils.VERTICAL_LINE],
-    ],
-  ],
+  OBLIQUES,
+  WIND,
+  DEACTIVATE,
+  SQUARES,
 ];
 
 export default SHAPES;
