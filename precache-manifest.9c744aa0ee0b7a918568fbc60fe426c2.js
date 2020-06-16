4115ab31ec645b3b6143851ed6286a4577e53f31
ArnaudSpanneut <arnaud.spanneut@gmail.com>
2020-06-17 00:39:09 +0200

    Updates

---
+++ precache-manifest.9c744aa0ee0b7a918568fbc60fe426c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdf43e71f7c078fb704eb143af5c1f0f",
+    "url": "/ClockClock24/index.html"
+  },
+  {
+    "revision": "4449a7641488d31dfb69",
+    "url": "/ClockClock24/static/css/main.6b4b3593.chunk.css"
+  },
+  {
+    "revision": "207a00eb09e23c178ea2",
+    "url": "/ClockClock24/static/js/2.72ee8c32.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ClockClock24/static/js/2.72ee8c32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4449a7641488d31dfb69",
+    "url": "/ClockClock24/static/js/main.bfa5f8b9.chunk.js"
+  },
+  {
+    "revision": "1c998f52bb426595c5db",
+    "url": "/ClockClock24/static/js/runtime-main.79fd1b57.js"
+  }
+]);
