9cedbebb7a582050c5d97afa849db69574672dcc
ArnaudSpanneut <arnaud.spanneut@gmail.com>
2020-07-05 17:30:13 +0200

    feat: display an info to rotate the phone

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,10 @@ import ClockClock24 from './components/ClockClock24/ClockClock24';
 
 ReactDOM.render(
   <div className="container">
+    <div className="rotate_info">
+      Rotate your phone for a better experience
+      <span role="img"aria-label="rotate"  className="rotate_info__emoji">🔄</span>
+    </div>,
     <ClockClock24/>
   </div>,
   document.getElementById('root'),
